61ff73617bb6701477a2d49aec2f652d8939ac87
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-09-14 05:08:54 +0000

    Stuff to find channels for Julian

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import apsw
 from flask import Flask
 import json
+import requests
 import subprocess
 import time
 
@@ -14,6 +15,54 @@ db = conn.cursor()
 @app.route("/")
 def hello_world():
    return "Hello World"
+
+@app.route("/recent_channels")
+def recent_channels():
+    now = int(time.time())
+    channels = db.execute("SELECT claim_hash FROM claim\
+                           WHERE claim_type = 2 AND creation_timestamp >= ?;",
+                           (now - 86400*3, )).fetchall()
+    channels = [c[0] for c in channels]
+
+    # Filter down to those whove published five streams
+    channels2 = []
+    for channel in channels:
+        count = db.execute("SELECT COUNT(claim_hash) FROM claim\
+                            WHERE channel_hash = ? AND claim_type = 1;",
+                           (channel, )).fetchone()[0]
+        if count >= 5:
+            channels2.append(channel)
+
+    # Now count the followers
+    claim_ids = [channel[::-1].hex() for channel in channels2]
+    claim_ids = ",".join(claim_ids)
+    response = requests.post("https://api.lbry.com/subscription/sub_count",
+                         data={"auth_token": "D18DoyrNVG6eAT1TTtzVbPqkiZoRAyPu",
+                               "claim_id": claim_ids})
+
+    if response.status_code != 200:
+        return json.dumps(dict(error="Something went wrong."))
+
+
+    followers = response.json()["data"]
+
+    # Filter again
+    channels3 = []
+    for i in range(len(followers)):
+        if followers[i] >= 20:
+            channels3.append(channels2[i])
+
+    result = []
+    stuff = db.execute(f"SELECT claim_name, claim_id FROM claim\
+                        WHERE claim_hash\
+                            IN ({','.join('?' for _ in channels3)})",
+                        channels3).fetchall()
+    for row in stuff:
+        result.append(dict(url="https://odysee.com/" + row[0] + ":"\
+                            + row[1]))
+
+    return tuple(result)
+
 
 @app.route("/frontpage/<claim_ids>/<page_size>")
 def get(claim_ids, page_size):
